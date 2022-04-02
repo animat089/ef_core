089caba962995cb625019f5548d79963d77514dc
Animesh Agarwal <animesh.agarwal@globallogic.com>
2022-04-02 17:13:51 +0530

    Updates to the Program.cs to remove unwanted components

--- src/Application.WithoutMigration/Program.cs
+++ src/Application.WithoutMigration/Program.cs
@@ -5,6 +5,5 @@ using Microsoft.EntityFrameworkCore;
 var connectionString = "Server=localhost;Initial Catalog=HogwartsDb;Integrated Security=True;";
 var contextOptions = new DbContextOptionsBuilder<HogwartsDbContext>().UseSqlServer(connectionString).Options;
 using var dbContext = new HogwartsDbContext(contextOptions);
-    dbContext.Database.Migrate();
     foreach (var course in dbContext.Courses)
         Console.WriteLine(course.Title);
