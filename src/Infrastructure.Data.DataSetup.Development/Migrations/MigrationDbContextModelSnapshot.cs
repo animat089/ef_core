38808ccbcc1cab9aee4f208811bc862d86fbed1c
Animesh Agarwal <58473075+animat089@users.noreply.github.com>
2022-08-18 13:41:21 +0000

    Adding the scripts for current changes

--- src/Infrastructure.Data.DataSetup.Development/Migrations/MigrationDbContextModelSnapshot.cs
+++ src/Infrastructure.Data.DataSetup.Development/Migrations/MigrationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Infrastructure.Data.DataSetup.Development.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.1")
+                .HasAnnotation("ProductVersion", "6.0.7")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
---
+++ src/Infrastructure.Data.DataSetup.Development/Scripts/Down/dev.1.0.2-dev.1.0.1.sql
@@ -0,0 +1,37 @@
+﻿BEGIN TRANSACTION;
+GO
+
+IF EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220807151128_dev.1.0.2')
+BEGIN
+    EXEC(N'UPDATE [Courses] SET [Title] = N''Course2''
+    WHERE [CourseId] = ''24e42ce8-6d38-4c5d-88e8-8310935bd886'';
+    SELECT @@ROWCOUNT');
+END;
+GO
+
+IF EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220807151128_dev.1.0.2')
+BEGIN
+    EXEC(N'UPDATE [Courses] SET [Title] = N''Course1''
+    WHERE [CourseId] = ''578c9088-d00d-421f-b418-bb3e305fa32f'';
+    SELECT @@ROWCOUNT');
+END;
+GO
+
+IF EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220807151128_dev.1.0.2')
+BEGIN
+    EXEC(N'UPDATE [Courses] SET [Title] = N''Course3''
+    WHERE [CourseId] = ''680b9bd7-cae9-4126-9b22-2c700fbab340'';
+    SELECT @@ROWCOUNT');
+END;
+GO
+
+IF EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220807151128_dev.1.0.2')
+BEGIN
+    DELETE FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20220807151128_dev.1.0.2';
+END;
+GO
+
+COMMIT;
+GO
+
