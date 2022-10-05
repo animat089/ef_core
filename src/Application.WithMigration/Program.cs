f451b8a91391cf37d67516ccd40c4d520541bdc7
Animesh Agarwal <58473075+animat089@users.noreply.github.com>
2022-10-05 22:45:45 +0530

    Updated the program to execute the stored proc

--- src/Application.WithMigration/Program.cs
+++ src/Application.WithMigration/Program.cs
@@ -7,11 +7,13 @@ var migrationAssemblyName = "AnimatLabs.Infrastructure.Data.DataSetup.Release";
 var contextOptions = new DbContextOptionsBuilder<HogwartsDbContext>().UseSqlServer(
     connectionString, x => x.MigrationsAssembly(migrationAssemblyName)).Options;
 using var dbContext = new HogwartsDbContext(contextOptions);
-dbContext.Database.Migrate();
-Console.WriteLine("All Courses from EF Model:");
-foreach (var course in dbContext.Courses)
-    Console.WriteLine(course.Title);
+{
+    dbContext.Database.Migrate();
+    Console.WriteLine("All Courses from EF Model:");
+    foreach (var course in dbContext.Courses)
+        Console.WriteLine(course.Title);
 
-Console.WriteLine("All Courses from Stored Proc:");
-foreach (var course in dbContext.Courses.FromSqlRaw("GetAllCourses"))
-    Console.WriteLine(course.Title);
+    Console.WriteLine("All Courses from Stored Proc:");
+    foreach (var course in dbContext.Courses.FromSqlRaw("GetAllCourses"))
+        Console.WriteLine(course.Title);
+}
